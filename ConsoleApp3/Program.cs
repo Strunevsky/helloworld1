02e84afa8869d9825bf515e6a12b99cb03ad54d4
Strunevskyj <Strunevskyj@gmail.com>
2022-02-07 08:49:32 +0300

    ConsoleApp3

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -11,6 +11,7 @@ namespace ConsoleApp3
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World");
+            Console.WriteLine("Oh,hi,Mike!");
             Console.ReadKey();
         }
     }
