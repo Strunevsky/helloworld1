9a59accb7ae636849781e3188176a4b725b08b46
Strunevsky <98808799+Strunevsky@users.noreply.github.com>
2022-02-08 13:09:00 +0300

    Update Program.cs

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -10,8 +10,37 @@ namespace ConsoleApp3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
-            Console.WriteLine("Oh,hi,Mike!");
+             double a=2, b=3,c ;
+            Console.WriteLine("Ввести А");
+            a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Ввести Б");
+            b = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Выполнить операцию +");
+            Console.WriteLine("Выполнить операцию -");
+            Console.WriteLine("Выполнить операцию *");
+            Console.WriteLine("Выполнить операцию /");
+            char rez = char.Parse(Console.ReadLine());
+            if (rez == '+')
+            {
+                c = a + b;
+                Console.WriteLine(c);
+
+            }
+           else if (rez == '-')
+            {
+                c = a - b;
+                Console.WriteLine(c);
+            }
+            else if (rez == '*')
+            {
+                c = a * b;
+                Console.WriteLine(c);
+            }
+            else if (rez == '/')
+            {
+                c = a / b;
+                Console.WriteLine(c);
+            }
             Console.ReadKey();
         }
     }
